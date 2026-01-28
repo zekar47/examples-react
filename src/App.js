38f20c078e9cfed47065e48ab172274071a94c1a
zekar <zekar@horsefucker.org>
2026-01-28 14:28:59 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mi App En React esta funcionando</h1>
+        <p>Has hecho click {count} veces</p>
+
+
+        <div>
+          <button onClick={ () => setCount(count + 1)}>
+            Increase
+            </button>
+          <button onClick={ () => setCount(count - 1)}>
+            Decrease
+            </button>
+        </div>
       </header>
     </div>
   );
